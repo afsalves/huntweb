0612f469291afa85d367644dff8082bdb298a850
Aline Alves <alinefsalvessi@gmail.com>
2020-12-16 08:41:15 -0300

    Inclusao pagina de detalhe de  produtos

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import Routes from './routes';
+
 import "./styles.css";
 
 import Header from "./components/Header";
@@ -7,7 +10,7 @@ import Main from './pages/main';
 const App = () =>(
     <div className="App">
       <Header />
-      <Main />
+      <Routes/>
     </div>
 );
 
--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,25 +1,51 @@
 import React, {Component} from 'react';
 import api from "../../services/api";
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 export default class Main extends Component {
 
     state = {
-        products: []
-    }
+        products: [],
+        productInfo:{},
+        page: 1
+    };
 
     componentDidMount(){
         this.loadProducts();
     }
 
     loadProducts = async () => {
-        const response = await api.get("/products");
+        const response = await api.get('/products?pages=${page}');
+
+        const { docs, ...productInfo } = response.data;
         
-        this.setState({ products: response.data.docs});
+        this.setState({ products: docs, productInfo, page });
     };
 
+    prevPage = () => {
+        const {page} = this.state;
+
+        if (page == 1) return;
+
+        const pageNumber = page - 1;
+
+        this.loadProducts(pageNumber);
+    };
+    nextPage = () => {
+        const {page, productInfo} = this.state;
+
+        if (page == productInfo.pages) return;
+
+        const pageNumber = page + 1;
+
+        this.loadProducts(pageNumber);
+    };
+   
+
     render () {
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return (
             <div className="product-list">
@@ -28,9 +54,17 @@ export default class Main extends Component {
                         <strong>{product.title}</strong>
                         <p>{product.description}</p>
 
-                        <a href="">Acessar</a>
+                        <Link to='/products/${product._id}'>Acessar</Link>
                     </article>
                 ))}
+                <div className="actions">
+                    <button disabled={page == 1} onClick={this.prevPage}>
+                        Anterior
+                    </button>
+                    <button disabled={productInfo.pages} onClick={this.nextPage}>
+                        Próxima
+                    </button>
+                </div>
             </div>
         );
     }
